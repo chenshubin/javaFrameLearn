930ce8bdfb4b55fe9cf82e7bc9166542dd89cd82
09067436-陈树斌 <09067436@hucais.com>
2020-10-13 14:36:40 +0800

    testgithub

---
+++ src/main/java/cn/murphy/springbootLearning/App.java
@@ -0,0 +1,25 @@
+package cn.murphy.springbootLearning;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello bin");
+
+        SpringApplication.run(App.class,args);
+    }
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "Hello bin";
+    }
+
+
+}
